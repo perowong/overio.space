106391d97ff5e740bb265f847e5ca5cb2b03b3ba
wangpeng <wangpeng@xiaoniangao.com>
2022-02-18 23:18:14 +0800

    overio.space

---
+++ gatsby-config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Overio`,
+    siteTitleAlt: `Overio - Overio Portfolio`,
+    siteHeadline: `Overio - Overio space`,
+    siteUrl: `https://overio.space`,
+    siteDescription: `massive knowledge over I/O`,
+    siteLanguage: `en`,
+    author: `@perowong`,
+    basePath: '/',
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `sections`,
+        path: `${__dirname}/src/sections`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        lessBabel: true,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-theme-ui`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Overio - Overio Portfolio`,
+        short_name: `Overio`,
+        description: `massive knowledge over I/O`,
+        start_url: `/`,
+        background_color: `#141821`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#f6ad55`,
+        display: `minimal-ui`,
+        icon: `static/app-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ].filter(Boolean),
+}
---
+++ src/components/footer.tsx
@@ -0,0 +1,39 @@
+/** @jsx jsx */
+import { Box, Flex, Link, useColorMode, jsx } from "theme-ui"
+
+const Footer = () => {
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
+
+  return (
+    <Box as="footer" variant="footer">
+      <button
+        sx={{ variant: `buttons.toggle`, fontWeight: `semibold`, display: `block`, mx: `auto`, mb: 3, borderRadius: 4 }}
+        onClick={toggleColorMode}
+        type="button"
+        aria-label="Toggle dark mode"
+      >
+        {isDark ? `Light` : `Dark`}
+      </button>
+      <Flex
+        sx={{
+          justifyContent: `center`,
+          alignItems: `center`,
+          mt: 3,
+          color: `text`,
+          // fontWeight: `semibold`,
+          a: { color: `#a1a1a1` },
+          fontSize: 12,
+          marginTop: 100
+        }}
+      >
+       <a href="http://beian.miit.gov.cn/">滇ICP备2022000793号</a>
+      </Flex>
+    </Box>
+  )
+}
+
+export default Footer
